c17299e866d1082b90831c69cf135275c4804c78
abhishek <abhishekulagadde@gmail.com>
2025-03-21 20:26:05 +0530

    Changes in Footer and Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -10,39 +10,33 @@ import Footer from './Components/Footer'
 import Dropbox from './Components/Dropbox'
 import ViewDoc from './Components/ViewDoc'
 import Chunks from './Components/Chunks'
+import { useLocation } from 'react-router-dom';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
+  
+    const location = useLocation();
+
   return (
-    <>
+    <div className="app-container">
       <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Login />}></Route>
-        <Route path="/home" element={<Home />} />
-        <Route path="/about/" element={<About />} />
-        <Route path="/viewdoc/:projectId" element={<ViewDoc />} />
-        <Route
-          path="/chunks/:projectId/:fileId/:chunkId"
-          element={<Chunks />}
-        />
-        <Route path="/dropbox" element={<Dropbox />} />
-      </Routes>
+      {/* Scrollable content section */}
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about/" element={<About />} />
+          <Route path="/viewdoc/:projectId" element={<ViewDoc />} />
+          <Route path="/chunks/:projectId/:fileId/:chunkId" element={<Chunks />} />
+          <Route path="/dropbox" element={<Dropbox />} />
+        </Routes>
+      </main>
+
       <Footer />
-    </>
-
-    //
-    //    <Home />
-
-    //   {/* <Navbar /> */}
-    //     <Dropbox />
-    //
-    //     <ViewDoc/>
-
-    //   <Footer />
-    // </>
+    </div>
   );
 }
 
