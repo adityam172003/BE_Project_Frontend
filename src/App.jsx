b6b42f2f016c6535330aa33ef715071e860ae87c
Kedar Pawar <pawarkedar363@gmail.com>
2025-03-21 12:35:31 +0530

    added routes to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Navbar from '../src/Components/Navbar'
 import Login from './Components/Login'
 import Home from './Components/Home'
+import About from './Components/About'
 import Footer from './Components/Footer'
 import Dropbox from './Components/Dropbox'
 import ViewDoc from './Components/ViewDoc'
@@ -22,9 +23,10 @@ function App() {
     <Routes>
       <Route path="/" element={<Login/>}></Route>
       <Route path="/home" element={<Home />} />
+      <Route path="/about/" element={<About/>}/>
       <Route path="/viewdoc/:projectId" element={<ViewDoc />} />
       <Route path="/chunks/:projectId/:fileId/:chunkId" element={<Chunks />} />
-
+      <Route path="/dropbox" element={<Dropbox/>}/>
     </Routes>
     <Footer />
   </>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
 
@@ -31,13 +31,13 @@ const Navbar = () => {
 
             <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
               <li>
-                <a>Homepage</a>
+              <Link to="/home"><a>Homepage</a></Link>
               </li>
               <li>
-                <a>About</a>
+              <Link to="/about"><a>About</a></Link>
               </li>
               <li>
-                <a>Upload</a>
+                <Link to="/dropbox"><a>Upload</a></Link>
               </li>
             </ul>
           </div>
