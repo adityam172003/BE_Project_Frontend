4232d7136818957867ac0781318c4c3499767332
Kedar Pawar <pawarkedar363@gmail.com>
2025-03-21 15:44:45 +0530

    fixed dropbox 2.0

--- src/Components/Dropbox.jsx
+++ src/Components/Dropbox.jsx
@@ -127,14 +127,14 @@ export default function Dropbox() {
               <textarea
                 className="textarea textarea-bordered textarea-primary w-full rounded-lg"
                 placeholder="Enter description of files"
-                rows="3"
+                rows="6"
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
               ></textarea>
             </div>
 
             {/* File Upload Area */}
-            <div className="mt-4 w-full flex justify-center">
+            {/* <div className="mt-4 w-full flex justify-center">
               <label
                 htmlFor="file-upload"
                 className="border-2 border-dashed border-gray-400 bg-base-200 rounded-xl p-10 w-full max-w-xs flex flex-col items-center justify-center cursor-pointer"
@@ -163,7 +163,7 @@ export default function Dropbox() {
                   <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" />
                 </svg>
               </label>
-            </div>
+            </div> */}
 
             {/* Display List of Selected Files (Horizontal + Wrap) */}
             {files.length > 0 && (
@@ -183,13 +183,17 @@ export default function Dropbox() {
             )}
            <div>
 
-<div className="p-4">
-  <button onClick={handleFolderSelect} className="px-4 py-2 mr-2 bg-blue-500 text-black rounded">
+<div className="p-4 ">
+  <div className="flex justify-around">
+  <button onClick={handleFolderSelect} className={folderHandle ? "w-full px-4 py-2 mr-2 bg-red-300 text-black rounded cursor-not-allowed" : "w-full px-4 py-2 mr-2 bg-blue-500 text-black rounded cursor-pointer"} disabled={!folderHandle}>
     Select Project Folder
   </button>
-  <button onClick={zipAndUpload} className="mt-4 px-4 py-2 ml-2 bg-green-500 text-black rounded">
+
+  <button onClick={zipAndUpload} className={folderHandle ? "w-full px-4 py-2 ml-2 bg-green-500 text-black rounded cursor-pointer" : "w-full px-4 py-2 ml-2 bg-red-300 text-black rounded cursor-not-allowed"} disabled={!folderHandle}>
     Upload zip ZIP
   </button>
+  </div>
+  
 
   {folderHandle && <p className="mt-2">Selected: {folderHandle.name}</p>}
 
@@ -199,9 +203,9 @@ export default function Dropbox() {
 
 </div>
             {/* Optional Submit Button */}
-            <div className="mt-6 flex justify-end">
+            {/* <div className="mt-6 flex justify-end">
               <button className="btn btn-primary">Submit</button>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
